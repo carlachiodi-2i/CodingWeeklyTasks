09bceec136891e5d2a532088138cd44b8c9f5b9b
Carla Chiodi <carla.chiodi@2itesting.com>
2022-05-26 14:28:04 +0100

    solve wordle

---
+++ Challenge5-BeatGame/cypress/integration/wordle.js
@@ -0,0 +1,77 @@
+/// <reference types="cypress" />
+
+function enterWord (word) {
+    word.split('').forEach(letter => {
+        cy.window().trigger('keydown', {key: letter})
+    })
+    cy.window().trigger('keydown', {key: 'Enter'}).wait(2000)
+}
+
+// count unique letters in the word
+function countUniqueLetters(word) {
+    return new Set(word).size
+}
+
+function tryNextWord (wordList) {
+    // Show the number of words in the wordlist, it should decrease in every run
+    console.log('word list with %d words', wordList.length)
+
+    // Enter a random word from list
+    const word = Cypress._.sample(wordList)
+    enterWord(word)
+    let count = 0
+    const seen = new Set()
+    // Check attributes of each letter (Absent, Present or Correct)
+    cy.get(`game-row[letters=${word}]`)
+        .find('game-tile')
+        .should('have.length', word.length)
+        .each(($tile, position) => {
+            const letter = $tile.attr('letter')
+            // only consider characters we see for first time
+            if (seen.has(letter)) {
+                return
+            }
+            seen.add(letter)
+            const evaluation = $tile.attr('evaluation')
+            console.log(letter, evaluation)
+
+            //Filter word list based on the evaluation
+            if (evaluation === 'absent') {
+                // Remove words that contain absent letter
+                wordList = wordList.filter((w) => !w.includes(letter))
+            } else if (evaluation === 'present') {
+                // Keep words that contain present letter
+                wordList = wordList.filter((w) => w.includes(letter))
+            } else if (evaluation === 'correct') {
+                // increase count
+                count += 1
+                wordList = wordList.filter((w) => w[position] === letter)
+            }
+    }).then(() => {
+        if (count ===  countUniqueLetters(word)) {
+            cy.log('**SOLVED**')
+        } else {
+            tryNextWord(wordList)
+        }
+    })
+
+}
+
+describe('Beat Wordle', () => {
+    it('Solve Wordle', () => {
+        // Get the list of words from Wordle Request
+        cy.intercept('GET', '**/main.*.js', (req) => {
+            req.continue((res) => {
+                res.body = res.body.replace('=["cigar', '=window.wordList=["cigar',)
+            })
+        }).as('words')
+        // cy.fixture('wordList.json').as('wordList')
+        cy.visit('/index.html').its('wordList').then((wordList) => {
+            //Close GDPR and Help windows
+            cy.get('#pz-gdpr-btn-closex').click().wait(1000)
+            cy.get('body > game-app').click().wait(1000)
+
+            tryNextWord(wordList)
+        })  
+    })
+})
